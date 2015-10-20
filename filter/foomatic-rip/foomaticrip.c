7b2499ba317bb1aad5245d98645e0dc9003a8cb5
Till Kamppeter <till.kamppeter@gmail.com>
2015-10-20 13:01:15 -0200

    foomatic-rip: Fixed string length for shell path constant.

--- filter/foomatic-rip/foomaticrip.c
+++ filter/foomatic-rip/foomaticrip.c
@@ -174,7 +174,7 @@ char cupsfilterpath[PATH_MAX] = "/usr/local/lib/cups/filter:"
                                 "/opt/cups/filter:"
                                 "/usr/lib/cups/filter";
 
-char modern_shell[64] = SHELL;
+char modern_shell[] = SHELL;
 
 void config_set_option(const char *key, const char *value)
 {
