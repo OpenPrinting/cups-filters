42985b8d48f79a481c2e9caed1880baaf0623dac
Till Kamppeter <till.kamppeter@gmail.com>
2016-07-08 17:03:21 -0300

    rastertops: Added copyright and license info.

--- filter/rastertops.c
+++ filter/rastertops.c
@@ -1,3 +1,25 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @brief Convert PWG Raster to a PostScript file
+ * @file rastertops.c
+ * @author Pranjal Bhor <bhor.pranjal@gmail.com> (C) 2016
+ * @author Neil 'Superna' Armstrong <superna9999@gmail.com> (C) 2010
+ * @author Tobias Hoffmann <smilingthax@gmail.com> (c) 2012
+ * @author Till Kamppeter <till.kamppeter@gmail.com> (c) 2014
+ */
+
 #include <config.h>
 #include <sys/types.h>
 #include <sys/stat.h>
