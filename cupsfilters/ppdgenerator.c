03c045ccc473b248caec52f86c504204ac8f7eb7
Till Kamppeter <till.kamppeter@gmail.com>
2018-01-26 16:10:11 +0100

    libcupsfilters: In PPD generator use same priority order for the different attributes which list color spaces as CUPS does.

--- cupsfilters/ppdgenerator.c
+++ cupsfilters/ppdgenerator.c
@@ -2325,8 +2325,8 @@ ppdCreateFromIPP(char   *buffer,	/* I - Filename buffer */
   * ColorModel...
   */
 
-  if ((attr = ippFindAttribute(response, "pwg-raster-document-type-supported", IPP_TAG_KEYWORD)) == NULL)
-    if ((attr = ippFindAttribute(response, "urf-supported", IPP_TAG_KEYWORD)) == NULL)
+  if ((attr = ippFindAttribute(response, "urf-supported", IPP_TAG_KEYWORD)) == NULL)
+    if ((attr = ippFindAttribute(response, "pwg-raster-document-type-supported", IPP_TAG_KEYWORD)) == NULL)
       if ((attr = ippFindAttribute(response, "print-color-mode-supported", IPP_TAG_KEYWORD)) == NULL)
         attr = ippFindAttribute(response, "output-mode-supported", IPP_TAG_KEYWORD);
 
