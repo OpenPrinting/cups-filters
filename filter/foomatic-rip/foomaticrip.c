64ca9c2fb5890f5cf5571b628dddc6d68ce3c929
Till Kamppeter <till.kamppeter@gmail.com>
2018-09-28 18:00:52 +0200

    foomatic-rip: Reset stdin after replacing the underlying file descriptor.

--- filter/foomatic-rip/foomaticrip.c
+++ filter/foomatic-rip/foomaticrip.c
@@ -663,6 +663,7 @@ int print_file(const char *filename, int convert)
                     rip_die(EXIT_PRNERR_NORETRY_BAD_SETTINGS,
                             "Couldn't dup stdout of pdf-to-ps\n");
 
+                clearerr(stdin);
                 ret = print_file("<STDIN>", 0);
 
                 wait_for_process(renderer_pid);
