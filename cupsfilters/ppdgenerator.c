10b3824ed1a24a7d67cb9ef149a43b2e603f4cea
Zdenek Dohnal <zdohnal@redhat.com>
2020-11-23 14:34:04 +0100

    ppdgenerator.c: Free printer_sizes array
    We need to free 'printer_sizes', which is allocated in 'ppdCreateFromIPP2()', instead of
    'sizes', which is passed as a parameter - because it solves a memory leak and it doesn't
    leave an invalid pointer.

--- cupsfilters/ppdgenerator.c
+++ cupsfilters/ppdgenerator.c
@@ -2353,8 +2353,6 @@ ppdCreateFromIPP2(char         *buffer,          /* I - Filename buffer */
 		     twidth, tlength);
     }
 
-    cupsArrayDelete(sizes);
-
    /*
     * Custom size support...
     */
@@ -2393,7 +2391,6 @@ ppdCreateFromIPP2(char         *buffer,          /* I - Filename buffer */
       cupsFilePuts(fp, "*CustomPageSize True: \"pop pop pop <</PageSize[5 -2 roll]/ImagingBBox null>>setpagedevice\"\n");
     }
   } else {
-    cupsArrayDelete(sizes);
     cupsFilePrintf(fp,
 		   "*%% Printer did not supply page size info via IPP, using defaults\n"
 		   "*OpenUI *PageSize/Media Size: PickOne\n"
@@ -2459,6 +2456,8 @@ ppdCreateFromIPP2(char         *buffer,          /* I - Filename buffer */
 		   "*PaperDimension EnvDL/Envelope DL: \"312 624\"\n"
 		   "*PaperDimension EnvMonarch/Envelope Monarch: \"279 540\"\n");
   }
+
+  cupsArrayDelete(printer_sizes);
 
  /*
   * InputSlot...
