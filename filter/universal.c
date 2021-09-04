95e60f96fefe0beacbe211bc43e0fde3d534e928
= <awesomepranshu@gmail.com>
2021-09-04 16:40:19 +0530

    Add a universal filter function to replace chain of individual filters

---
+++ filter/universal.c
@@ -0,0 +1,75 @@
+/*
+ * Include necessary headers...
+ */
+
+#include <cupsfilters/filter.h>
+#include <signal.h>
+
+
+/*
+ * Local globals...
+ */
+
+static int		JobCanceled = 0;/* Set to 1 on SIGTERM */
+
+
+/*
+ * Local functions...
+ */
+
+static void		cancel_job(int sig);
+
+
+/*
+ * 'main()' - Main entry.
+ */
+
+int					/* O - Exit status */
+main(int  argc,				/* I - Number of command-line args */
+     char *argv[])			/* I - Command-line arguments */
+{
+  int           ret;
+  filter_input_output_format_t input_output_format;
+#if defined(HAVE_SIGACTION) && !defined(HAVE_SIGSET)
+  struct sigaction action;		/* Actions for POSIX signals */
+#endif /* HAVE_SIGACTION && !HAVE_SIGSET */
+
+ /*
+  * Register a signal handler to cleanly cancel a job.
+  */
+
+#ifdef HAVE_SIGSET /* Use System V signals over POSIX to avoid bugs */
+  sigset(SIGTERM, cancel_job);
+#elif defined(HAVE_SIGACTION)
+  memset(&action, 0, sizeof(action));
+
+  sigemptyset(&action.sa_mask);
+  action.sa_handler = cancel_job;
+  sigaction(SIGTERM, &action, NULL);
+#else
+  signal(SIGTERM, cancel_job);
+#endif /* HAVE_SIGSET */
+
+  input_output_format.input_format = getenv("CONTENT_TYPE");
+  input_output_format.output_format = getenv("FINAL_CONTENT_TYPE");
+  ret = filterCUPSWrapper(argc, argv, universal, &input_output_format , &JobCanceled);
+
+  if (ret)
+    fprintf(stderr, "ERROR: universal filter failed.\n");
+
+  return (ret);
+}
+
+
+/*
+ * 'cancel_job()' - Flag the job as canceled.
+ */
+
+static void
+cancel_job(int sig)			/* I - Signal number (unused) */
+{
+  (void)sig;
+
+  JobCanceled = 1;
+}
+
