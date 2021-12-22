1786a28cc644ec01ec67ece4cd9bb59e1bd52731
Till Kamppeter <till.kamppeter@gmail.com>
2021-12-22 19:17:53 -0300

    libcupsfilters: Removed pdftoippprinter.[ch]
    As we have filter functions replacing practically all filters and can
    call one or another external CUPS filter executable (driver) or even
    backends via filterExternalCUPS(), we do not need any more the
    apply_filters() API function to execute chains of external CUPS filter
    executables. Therefore we remove this function.

--- cupsfilters/pdftoippprinter.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CUPS_FILTERS_PDFTOIPPPRINTER_H
-#  define _CUPS_FILTERS_PDFTOIPPPRINTER_H
-
-int         apply_filters(int argc, char *argv[]);
-void		set_option_in_str(char *buf, int buflen,
-					  const char *option,
-					  const char *value);
-
-#endif
