0e54efce8b6b3649c4dbcf72c6dca2b6021893d9
Till Kamppeter <till.kamppeter@gmail.com>
2022-03-27 23:07:21 +0200

    libcupsfilters: In mupdftoraster() removed debugging change
    A change done for debugging slipped into the previous commit. Fixed.

--- cupsfilters/mupdftoraster.c
+++ cupsfilters/mupdftoraster.c
@@ -522,7 +522,7 @@ mupdftoraster (int inputfd,         /* I - File descriptor input stream */
      Raster header then, no extra manipulation needed.
      From the header h only cupsWidth/cupsHeight (dimensions in pixels),
      resolution, and color space are used here. */
-  cupsRasterPrepareHeader(&h, data, OUTPUT_FORMAT_CUPS_RASTER,
+  cupsRasterPrepareHeader(&h, data, OUTPUT_FORMAT_PWG_RASTER,
 			  OUTPUT_FORMAT_PWG_RASTER, 1, &cspace);
 
   if ((h.HWResolution[0] == 100) && (h.HWResolution[1] == 100)) {
