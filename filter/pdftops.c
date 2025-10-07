e3838f551ae926b885ce66b735329a81ebb7c6c4
ValdikSS <iam@valdikss.org.ru>
2025-10-07 21:59:20 +0300

    Clarify Poppler grayscale warning message. Fixes #640
    Many printer queue viewers show only the latest status message
    while printing, which results in confusing message when using
    Poppler pdftops backend when printing:
    
    >Use "pdftops-renderer" option (see cups-filters README file) to use Ghostscript or MuPDF for the PDF -> PostScript conversion.
    
    And the first line is omitted.
    
    Simplify the text and add more details into cups-filters readme.

--- filter/pdftops.c
+++ filter/pdftops.c
@@ -924,8 +924,7 @@ main(int  argc,				/* I - Number of command-line args */
       /* pdf_argv[1] = (char *)"-level1";
 	 pdf_argv[pdf_argc++] = (char *)"-optimizecolorspace"; */
       /* Issue a warning message when printing a grayscale job with Poppler */
-      fprintf(stderr, "WARNING: Grayscale/monochrome printing requested for this job but Poppler is not able to convert to grayscale/monochrome PostScript.\n");
-      fprintf(stderr, "WARNING: Use \"pdftops-renderer\" option (see cups-filters README file) to use Ghostscript or MuPDF for the PDF -> PostScript conversion.\n");
+      fprintf(stderr, "WARNING: Grayscale/monochrome could be printed as color with Poppler. See cups-filters README file (\"pdftops-renderer\" option).\n");
     }
     pdf_argv[pdf_argc++] = filename;
     pdf_argv[pdf_argc++] = (char *)"-";
