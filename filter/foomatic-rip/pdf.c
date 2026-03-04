f1ebd70ebcd7495283c8fd3056e41a22bcd4bb83
Rohit Kumar <149245736+rkt0209@users.noreply.github.com>
2026-03-04 09:01:13 +0530

    Add warning log for non-zero exit status of ghostscript
    Log a warning if ghostscript process exits with a non-zero status.

--- filter/foomatic-rip/pdf.c
+++ filter/foomatic-rip/pdf.c
@@ -146,7 +146,9 @@ pdf_extract_pages(char filename[PATH_MAX],
   FILE *pd = popen(gscommand, "r");
   if (!pd)
     rip_die(EXIT_STARVED, "Could not run ghostscript to extract the pages!\n");
-  pclose(pd);
+  
+  if (pclose(pd) != 0)
+    _log("Warning: ghostscript process exited with non-zero status\n");
 
   return (1);
 }
